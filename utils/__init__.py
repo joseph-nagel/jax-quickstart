ba84c00083a5d1ee4c74a8fdf3600dcb7caf6314
Joseph Nagel <JosephBNagel@gmail.com>
2026-01-13 23:29:45 +0100

    Minor

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,10 @@
 '''Utilities.'''
 
 from .data import make_linear_data
-
 from .functions import (
     sigmoid,
     sigmoid_sum,
     sigmoid_deriv
 )
-
 from .modules import MLP, AutoEncoder
-
 from .training import mse
